26fcd50b2899694cb3f09a5aef904335c4816fad
Rosaline elrabie <Rosaline@vodafone.com>
2023-03-16 15:00:50 +0100

    commit first react application

--- src/features/book/book.js
+++ src/features/book/book.js
@@ -2,33 +2,33 @@ import ChangeShelfOptions from "../changeShelfOptions/changeShelfOptions";
 import "./book.css";
 
 const Book = (props) => {
-
   const changeBookStatus = (status, book) => {
-    props.updateBookStauts(status,book)
-  }
+    props.updateBookStauts(status, book);
+  };
 
-return (
+  return (
     <div>
-        <div className="book">
-                        <div className="book-top">
-                          <div
-                            className="book-cover"
-                            style={{
-                              width: 128,
-                              height: 193,
-                              backgroundImage:
-                                `url(${props.imageUrl})`,
-                            }}
-                          ></div>
-                          <ChangeShelfOptions book={props.book} shelf={props.shelf} changeBookStatus= {changeBookStatus}></ChangeShelfOptions>
-                        </div>
-                        <div className="book-title"> {props.title} </div>
-                        <div className="book-authors">{props.author}</div>
-                      </div>
-     
-     
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${props.imageUrl})`,
+            }}
+          ></div>
+          <ChangeShelfOptions
+            book={props.book}
+            shelf={props.shelf}
+            changeBookStatus={changeBookStatus}
+          ></ChangeShelfOptions>
+        </div>
+        <div className="book-title"> {props.title} </div>
+        <div className="book-authors">{props.author}</div>
+      </div>
     </div>
-)
-}
+  );
+};
 
 export default Book;
